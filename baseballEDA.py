68fa3cfe0f8f575175aefc420571d87248a5f4b1
Anthony Vallejo <69154460+an2nyv@users.noreply.github.com>
2023-11-29 01:43:14 -0500

    reads batting data

--- baseballEDA.py
+++ baseballEDA.py
@@ -1,3 +1,12 @@
-intro = "hello world"
-print(intro)
+import pandas as pd
+import os
+
+new_dirr = "/Users/Anthony Vallejo/OneDrive/Desktop/Personal_Projects/DATA/baseballData"
+
+os.chdir(new_dirr)
+
+current_directory = os.getcwd()
+print(current_directory)
+
+battingData = pd.read_csv('Batting.csv')
 
